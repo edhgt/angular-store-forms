0982ceaf062807237d2cb1a706cc18b14ffff8a3
Elmer Danilo Hernandez <eldh.dev@gmail.com>
2023-04-26 09:48:39 -0600

    usando FormGroup para agrupar campos

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validator, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,18 +8,19 @@ import { FormControl, Validator, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(10)
-  ]);
-  emailField = new FormControl('example@email');
-  phoneField = new FormControl('12345678');
-  colorField = new FormControl('#000000');
-  dateField = new FormControl();
-  numberField = new FormControl(12);
-
-  categoryField = new FormControl('category-3');
-  tagField = new FormControl();
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    age: new FormControl(12),
+    category: new FormControl(''),
+    tag: new FormControl(''),
+    agree: new FormControl(false),
+    gender: new FormControl(''),
+    zone: new FormControl(''),
+  });
 
   constructor() { }
 
@@ -27,19 +28,67 @@ export class BasicFormComponent implements OnInit {
     this.nameField.valueChanges
     .subscribe(value => {
       console.log(value);
-    })
+    });
   }
 
   getNameValue() {
     console.log(this.nameField.value);
   }
 
+  save(event) {
+    console.log(this.form.value);
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
   get isNameFieldValid() {
-    return this.nameField.touched && this.nameField.invalid;
+    return this.nameField.touched && this.nameField.valid;
   }
 
   get isNameFieldInvalid() {
     return this.nameField.touched && this.nameField.invalid;
+  }
+
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get phoneField() {
+    return this.form.get('phone');
+  }
+
+  get colorField() {
+    return this.form.get('color');
+  }
+
+  get dateField() {
+    return this.form.get('date');
+  }
+
+  get ageField() {
+    return this.form.get('age');
+  }
+
+  get categoryField() {
+    return this.form.get('category');
+  }
+
+  get tagField() {
+    return this.form.get('tag');
+  }
+
+  get agreeField() {
+    return this.form.get('agree');
+  }
+
+  get genderField() {
+    return this.form.get('gender');
+  }
+
+  get zoneField() {
+    return this.form.get('zone');
   }
 
 }
